36240936d565c9d1666bea74baa5e2f36274bff2
Romain KOENIG <romain@kromatic.fr>
2022-11-07 17:34:57 +0100

    First steps of refactoring
    Moving files around, and getting to a point where it's functionning exactly as before.

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+// Eleventy config file
+
+module.exports = function (eleventyConfig) {
+
+	eleventyConfig.addPassthroughCopy("./src/css/");
+	eleventyConfig.addPassthroughCopy("./src/js/");
+
+	eleventyConfig.addWatchTarget("./src/js/");
+	eleventyConfig.addWatchTarget("./src/css/");
+
+	eleventyConfig.addPassthroughCopy("src/pics");
+	eleventyConfig.addPassthroughCopy("src/favicon");
+
+	return {
+		markdownTemplateEngine: "njk",
+		dir: {
+			input: "src",
+			output: "public",
+		},
+	};
+};
